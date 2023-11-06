65082a4c80a61f4f0bef0ae47d856484f2027712
agastonsosa <138701662+agastonsosa@users.noreply.github.com>
2023-11-06 00:03:35 +0000

    changes on footer2

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faTwitter,
+  faFacebook,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
 
 export const Footer = () => (
-  <footer
-    className="footer"
-    style={{ backgroundColor: "rgba(65, 94, 76, 1)" }}
-  >
+  <footer className="footer" style={{ backgroundColor: "rgba(65, 94, 76, 1)" }}>
     <Row className="p-5">
       <Col>
         <img
@@ -34,9 +38,23 @@ export const Footer = () => (
     </Row>
     <div className="line2"></div>
 	<Row>
-		<Col className="ms-5">
-		<p className="text-white fs-6">2023 AlmaCena All rights Reserved.</p></Col>
-		<Col></Col>
-	</Row>
+      <Col lg="4">
+        <p className="text-white fs-6">2023 AlmaCena All rights Reserved.</p>
+      </Col>
+      <Col lg="8">
+        <a href="https://www.instagram.com" className="text-white p-2">
+          <FontAwesomeIcon icon={faInstagram} size="2x" />
+        </a>
+        <a href="https://www.twitter.com" className="text-white p-2">
+          <FontAwesomeIcon icon={faTwitter} size="2x" />
+        </a>
+        <a href="https://www.facebook.com" className="text-white p-2">
+          <FontAwesomeIcon icon={faFacebook} size="2x" />
+        </a>
+        <a href="https://www.youtube.com" className="text-white p-2">
+          <FontAwesomeIcon icon={faYoutube} size="2x" />
+        </a>
+      </Col>
+    </Row>
   </footer>
 );

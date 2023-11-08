55e86fe2d7890ddd28131b3cb1e9dfa7ce98db8c
SaraPe <135864591+sarap29@users.noreply.github.com>
2023-11-08 17:53:06 +0000

    se actualiza app.py

--- src/api/utils.py
+++ src/api/utils.py
@@ -21,21 +21,42 @@ def has_no_empty_params(rule):
     return len(defaults) >= len(arguments)
 
 def generate_sitemap(app):
-    links = ['/admin/']
+    unique_endpoints = set()
     for rule in app.url_map.iter_rules():
-        # Filter out rules we can't navigate to in a browser
-        # and rules that require parameters
-        if "GET" in rule.methods and has_no_empty_params(rule):
+        if has_no_empty_params(rule):
             url = url_for(rule.endpoint, **(rule.defaults or {}))
-            if "/admin/" not in url:
-                links.append(url)
+            if url != "/" and "/admin/" not in url:
+                unique_endpoints.add(url)
 
-    links_html = "".join(["<li><a href='" + y + "'>" + y + "</a></li>" for y in links])
-    return """
+    links_html = "".join(["<li style='padding: 10px 0 5px 0;'><a style='text-decoration: none; color: white; text-transform: uppercase;' href='" + y +
+                         "' onmouseover='this.style.color=\"darkred\"' onmouseout='this.style.color=\"white\"' onclick='this.style.color=\"blue\"'>" + y + '</a></li>' for y in unique_endpoints])
+    return f"""
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>AlmaCena API</title>
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@700&family=Montserrat&family=Pixelify+Sans&display=swap" rel="stylesheet">
+        </head>
+        <body style="background-color: #415e4c; color: white; text-align: center; font-family: 'Montserrat', arial;">
         <div style="text-align: center;">
-        <img style="max-height: 80px" src='https://storage.googleapis.com/breathecode/boilerplates/rigo-baby.jpeg' />
-        <h1>Rigo welcomes you to your API!!</h1>
-        <p>API HOST: <script>document.write('<input style="padding: 5px; width: 300px" type="text" value="'+window.location.href+'" />');</script></p>
-        <p>Start working on your project by following the <a href="https://start.4geeksacademy.com/starters/full-stack" target="_blank">Quick Start</a></p>
-        <p>Remember to specify a real endpoint path like: </p>
-        <ul style="text-align: left;">"""+links_html+"</ul></div>"
+        <img style="max-height: 600px" src="https://i.ibb.co/16FC60X/logoalmacena.png" />
+        <div style="position: fixed; bottom: 0; right: 0; margin: 40px;">
+                <button style="font-family: 'Barlow', sans-serif; border-radius: 40px;background-color: grey; padding: 20px; box-shadow: 0px 0px 10px 0px white; transition: all 0.3s ease;"
+                    onmouseover="this.style.backgroundColor='rgba(65, 94, 76, 1)'; this.style.boxShadow='0px 0px 20px 0px white';"
+                    onmouseout="this.style.backgroundColor='grey'; this.style.boxShadow='0px 0px 10px 0px white';">
+                    <a style="text-decoration: none; font-size: 20px; color: white;" href="/admin">ADMIN MODE</a>
+                </button>
+         </div>
+        <h1>Welcome to AlmaCena API</h1>
+         <p style="font-size:25px;">API HOST <script>document.write('<input style="padding: 10px; margin-left: 20px; width: 300px" type="text" value="'+window.location.href+'" />');</script></p>
+        <h2 style="margin: 300px 0px 10px 0px; font-size:60px;">ENDPOINTS</h2>
+        <div>
+        <ul style="text-align: center; font-size: 45px; list-style-type: none; padding-right:30px; margin-bottom: 300px;">{links_html}</ul>
+        </div>
+        </body>
+        </html>
+        """
--- src/app.py
+++ src/app.py
@@ -1,25 +1,34 @@
 """
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
+
+ALMACENA SERVER v2.0
+
 """
+
 import os
 from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from api.utils import APIException, generate_sitemap
-from api.models import db
+from api.models import db, User, MateriasPrimas, UserMateriasPrimas, Receta, UserReceta, IngredientesReceta, UserProductoFinal
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
 
-#from models import Person
+
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-# database condiguration
+
+# DATABASE Configuration
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
@@ -30,41 +39,235 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db, compare_type = True)
 db.init_app(app)
 
-# Allow CORS requests to this API
 CORS(app)
-
-# add the admin
 setup_admin(app)
-
-# add the admin
 setup_commands(app)
+bcrypt = Bcrypt(app)
 
-# Add all endpoints form the API with a "api" prefix
 app.register_blueprint(api, url_prefix='/api')
 
-# Handle/serialize errors like a JSON object
+# HANDLE ERRORS
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+# ENDPOINTS | HOME
+
 @app.route('/')
 def sitemap():
     if ENV == "development":
         return generate_sitemap(app)
     return send_from_directory(static_file_dir, 'index.html')
 
-# any other endpoint will try to serve it like a static file
-@app.route('/<path:path>', methods=['GET'])
-def serve_any_other_file(path):
-    if not os.path.isfile(os.path.join(static_file_dir, path)):
-        path = 'index.html'
-    response = send_from_directory(static_file_dir, path)
-    response.cache_control.max_age = 0 # avoid cache memory
-    return response
+# ENDPOINTS | VER TODOS LOS USERS
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([{"id": user.id, "username": user.name} for user in users]), 200
+
+# ENDPOINTS | VER INFO DE USER
+
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        raise APIException("User not found", status_code=404)
+    return jsonify(user.serialize()), 200
 
 
-# this only runs if `$ python src/main.py` is executed
+# USER | SIGNUP #
+
+
+@app.route("/signup", methods=["POST"])
+def signup():
+    data = request.get_json()
+    app.logger.info("Data received from signup endpoint: %s", data) 
+    email = data.get("email")
+    password = data.get("password")
+    name = data.get("name")
+    last_name = data.get("last_name")
+    address= data.get("address")
+
+    existing_user = User.query.filter_by(email=email).first()
+    if existing_user:
+        return jsonify({"msg": "Email already exists"}), 400
+
+    new_user = User(
+        email=email,
+        password=bcrypt.generate_password_hash(password).decode('utf-8'),
+        name=name,
+        last_name=last_name,
+        address=address
+    )
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    access_token = create_access_token(identity=new_user.id)
+
+    return jsonify({"token": access_token, "user_id": new_user.id}), 201
+
+
+# USER | LOGIN #
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    email = data.get("email")
+    password = data.get("password")
+
+    user = User.query.filter_by(email=email).first()
+
+    if user and bcrypt.check_password_hash(user.password, password):
+        # CORRECT PASS=> GENERATE TOKEN
+        access_token = create_access_token(identity=user.id)
+        return jsonify({"token": access_token, "user_id": user.id}), 200
+    else:
+        # USER NOT FOUND OR INCORRECT PASS
+        return jsonify({"msg": "Bad username or password"}), 401
+
+
+# USER | LOGOUT #
+
+
+@app.route("/logout", methods=["POST"])
+@jwt_required()
+def logout():
+    return jsonify({"msg": "Logout successful"}), 200
+
+
+# ENDPOINT | MATERIAS PRIMAS DEL USUARIO
+
+@app.route('/<int:user_id>/dashboard/ingredients', methods=['GET'])
+# @jwt_required()
+def get_user_ingredients(user_id):
+    user_materias_primas = UserMateriasPrimas.query.filter_by(user_id=user_id).all()
+
+    ingredients_list = []
+    for user_materia_prima in user_materias_primas:
+        materia_prima = MateriasPrimas.query.get(user_materia_prima.materias_primas_id)
+        if materia_prima:
+            ingredient_data = {
+                "materia_prima_id": user_materia_prima.materias_primas_id,
+                "nombre": materia_prima.nombre,
+                "cantidad_stock": user_materia_prima.cantidad_stock
+            }
+            ingredients_list.append(ingredient_data)
+
+    return jsonify(ingredients_list), 200
+
+# ENDPOINT | PRODUCTOS DEL USUARIO
+
+@app.route('/<int:user_id>/dashboard/products', methods=['GET'])
+# @jwt_required()
+def get_user_products(user_id):
+    user_products = UserProductoFinal.query.filter_by(user_id=user_id).all()
+
+    products_list = []
+    for user_product in user_products:
+        product_data = {
+            "receta_id": user_product.receta_id,
+            "nombre": user_product.receta_relationship.nombre,
+            "cantidad_inventario": user_product.cantidad_inventario
+        }
+        products_list.append(product_data)
+
+    return jsonify(products_list), 200
+
+# ENDPOINT | RECETAS DEL USUARIO
+
+@app.route('/<int:user_id>/dashboard/recipes', methods=['GET'])
+# @jwt_required()
+def get_user_recipes(user_id):
+    user_recetas = UserReceta.query.filter_by(user_id=user_id).all()
+
+    recipes_list = []
+    for user_receta in user_recetas:
+        recipe_data = {
+            "receta_id": user_receta.receta_id,
+            "nombre": user_receta.receta_relationship.nombre,
+            "rinde": user_receta.receta_relationship.rinde
+        }
+        recipes_list.append(recipe_data)
+
+    return jsonify(recipes_list), 200
+
+# ENDPOINT | SINGLE RECIPE DE UN USUARIO
+
+@app.route('/<int:user_id>/dashboard/recipes/<int:recipe_id>', methods=['GET'])
+# @jwt_required()
+def get_user_recipe(user_id, recipe_id):
+    user_receta = UserReceta.query.filter_by(user_id=user_id, receta_id=recipe_id).first()
+
+    if user_receta is None:
+        return jsonify({"error": "Receta no encontrada para el usuario especificado"}), 404
+
+    receta_info = {
+        "receta_id": user_receta.receta_id,
+        "nombre": user_receta.receta_relationship.nombre,
+        "rinde": user_receta.receta_relationship.rinde,
+        "ingredientes": []
+    }
+
+    ingredientes_receta = IngredientesReceta.query.filter_by(receta_id=recipe_id).all()
+    for ingrediente in ingredientes_receta:
+        materia_prima = MateriasPrimas.query.get(ingrediente.materias_primas_id)
+        if materia_prima:
+            ingrediente_data = {
+                "materia_prima_id": ingrediente.materias_primas_id,
+                "nombre": materia_prima.nombre,
+                "cantidad_necesaria": ingrediente.cantidad_necesaria,
+                "unidad_medida": materia_prima.unidad_medida
+            }
+            receta_info["ingredientes"].append(ingrediente_data)
+
+    return jsonify(receta_info), 200
+
+
+ # ENDPOINT | DASHBOARD DE UN USUARIO
+
+@app.route('/<int:user_id>/dashboard', methods=['GET'])
+# @jwt_required()
+def get_user_dashboard(user_id):
+    user_ingredientes = UserMateriasPrimas.query.filter_by(user_id=user_id).all()
+    user_productos = UserProductoFinal.query.filter_by(user_id=user_id).all()
+
+    ingredientes_list = []
+    for user_ingrediente in user_ingredientes:
+        if user_ingrediente.cantidad_stock <= user_ingrediente.minimo_stock:
+            materia_prima = MateriasPrimas.query.get(user_ingrediente.materias_primas_id)
+            if materia_prima:
+                ingrediente_data = {
+                    "materia_prima_id": user_ingrediente.materias_primas_id,
+                    "nombre": materia_prima.nombre,
+                    "cantidad_stock": user_ingrediente.cantidad_stock,
+                    "unidad_medida": materia_prima.unidad_medida,
+                    "minimo_stock": user_ingrediente.minimo_stock
+                }
+                ingredientes_list.append(ingrediente_data)
+
+    productos_list = []
+    for user_producto in user_productos:
+        if user_producto.cantidad_inventario <= user_producto.cantidad_inventario_minimo:
+            producto = UserProductoFinal.query.get(user_producto.id)
+            if producto:
+                receta = Receta.query.get(producto.receta_id)
+                if receta:
+                    producto_data = {
+                        "producto_final_id": user_producto.id,
+                        "nombre": receta.nombre,
+                        "cantidad_inventario": user_producto.cantidad_inventario,
+                        "unidad_medida": receta.unidad_medida,
+                        "clasificacion": user_producto.clasificacion,
+                        "cantidad_inventario_minimo": user_producto.cantidad_inventario_minimo     
+                    }
+                    productos_list.append(producto_data)
+
+    return jsonify({"ingredientes": ingredientes_list, "productos_finales": productos_list}), 200
+
+##################################################################################################################################
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3001))
     app.run(host='0.0.0.0', port=PORT, debug=True)

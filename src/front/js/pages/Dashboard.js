54a97b0c666fb7725ce3c5e7fce13e15cda5d811
agastonsosa <138701662+agastonsosa@users.noreply.github.com>
2023-11-09 22:56:28 +0000

    dashboard looking even better

--- src/front/js/pages/Dashboard.js
+++ src/front/js/pages/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
-import { Card, ListGroup, Container, Row, Col, Alert } from "react-bootstrap";
+import { Card, ListGroup, Container, Row, Col, Alert, CardTitle, CardFooter } from "react-bootstrap";
+import "../../styles/dashboard.css";
 
 const Dashboard = () => {
   const { actions, store } = useContext(Context);
@@ -31,51 +32,55 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <div className="gray-bg">
-      <Container className="mt-5">
-        <h4 className="mb-4">Dashboard</h4>
-        <Row>
-          <Col md={6}>
-            <div className="fs-5 mb-3">Ingredientes para Comprar</div>
-            {ingredientes.length > 0 ? (
-              <Card className="rounded">
-                <ListGroup variant="flush">
-                  {ingredientes.map(ingrediente => (
-                    <div key={ingrediente.materia_prima_id} className="divider-line">
-                      <ListGroup.Item>
-                        {ingrediente.nombre} - {ingrediente.cantidad_stock}{" "}
-                        {ingrediente.unidad_medida}
-                      </ListGroup.Item>
-                    </div>
-                  ))}
-                </ListGroup>
-              </Card>
-            ) : (
-              <Alert variant="info">No tienes ningún ingrediente que comprar.</Alert>
-            )}
-          </Col>
-          <Col md={6}>
-            <div className="fs-5 mb-3">Productos Finales para Producir</div>
-            {productosFinales.length > 0 ? (
-              <Card className="rounded">
-                <ListGroup variant="flush">
-                  {productosFinales.map(producto => (
-                    <div key={producto.producto_final_id} className="divider-line">
-                      <ListGroup.Item>
-                        {producto.nombre} - {producto.cantidad_inventario}{" "}
-                        {producto.unidad_medida}
-                      </ListGroup.Item>
-                    </div>
-                  ))}
-                </ListGroup>
-              </Card>
-            ) : (
-              <Alert variant="info">No tienes que elaborar ningún producto por el momento.</Alert>
-            )}
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <Container className="mt-5">
+      <h4 className="mb-4 text-black text-start">Welcome</h4>
+      <Row>
+        <Col md={6}>
+          {ingredientes.length > 0 ? (
+            <Card className="rounded mb-5">
+              <CardTitle className="p-4">You are low on these ingredients:</CardTitle>
+              <ListGroup variant="flush">
+                {ingredientes.map(ingrediente => (
+                  <div key={ingrediente.materia_prima_id} className="divider-line">
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>{ingrediente.nombre}</Col>
+                        <Col>{ingrediente.cantidad_stock} {ingrediente.unidad_medida}</Col>
+                      </Row>
+                    </ListGroup.Item>
+                  </div>
+                ))}
+              </ListGroup>
+              <CardFooter>And that's all</CardFooter>
+            </Card>
+          ) : (
+            <Alert variant="info">Nothing low for now.</Alert>
+          )}
+        </Col>
+        <Col md={6}>
+          {productosFinales.length > 0 ? (
+            <Card className="rounded">
+              <CardTitle className="p-4">You are low on these products:</CardTitle>
+              <ListGroup variant="flush">
+                {productosFinales.map(producto => (
+                  <div key={producto.producto_final_id} className="divider-line">
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>{producto.nombre}</Col>
+                        <Col>{producto.cantidad_inventario} {producto.unidad_medida}</Col>
+                      </Row>
+                    </ListGroup.Item>
+                  </div>
+                ))}
+              </ListGroup>
+              <CardFooter>And that's all</CardFooter>
+            </Card>
+          ) : (
+            <Alert variant="info">Nothing low for now.</Alert>
+          )}
+        </Col>
+      </Row>
+    </Container>
   );
 };
 

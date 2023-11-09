d36a8e0d2aca74e96ae7a42df6d48011e72a5d96
agastonsosa <138701662+agastonsosa@users.noreply.github.com>
2023-11-09 18:56:15 +0000

    changes on link for button on navbar comp

--- src/front/js/component/LoginButton.js
+++ src/front/js/component/LoginButton.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const LoginButton = () => {
   return (
     <Button variant="light" className="login-button text-white">
-      Login
+      <Link to="/login" className='text-white'>Login</Link>
     </Button>
   );
 };
--- src/front/js/component/SignUpButton.js
+++ src/front/js/component/SignUpButton.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 
 
 
 const SignUpButton = () => {
   return (
     <Button variant="outline-light" className='signupbutton text-white'>
-      Sign up
+      <Link to="/signup" className='text-white'>Sign up</Link>
     </Button>
   );
 };

57571a2db5aed30e5d60d8980f33213be5606390
agastonsosa <138701662+agastonsosa@users.noreply.github.com>
2023-11-11 15:17:48 +0000

    ingredients view seems ok

--- src/front/js/pages/Ingredients.js
+++ src/front/js/pages/Ingredients.js
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "../store/appContext";
+import { Card, Container, Table } from "react-bootstrap";
 
 const Ingredients = () => {
-  return (
-    <div>Ingredients</div>
-  )
-}
+  const { actions, store } = useContext(Context);
+  const [materiasPrimas, setMateriasPrimas] = useState([]);
 
-export default Ingredients
+  useEffect(() => {
+    const fetchIngredientsData = async () => {
+      try {
+        const response = await fetch(process.env.BACKEND_URL + "/dashboard/ingredients", {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("jwt-token")}`
+          }
+        });
+
+        if (!response.ok) {
+          throw new Error("Error fetching ingredients data");
+        }
+
+        const data = await response.json();
+
+        if (data && Array.isArray(data) && data.length > 0) {
+          setMateriasPrimas(data);
+        }
+      } catch (error) {
+        console.error("Error fetching ingredients data:", error);
+      }
+    };
+
+    fetchIngredientsData();
+  }, []);
+
+  return (
+    <Container>
+    <Card className="rounded my-5">
+      <Card.Header>
+        <Card.Title className="mb-0">Materias Primas</Card.Title>
+      </Card.Header>
+      <Card.Body>
+        <Table striped bordered hover responsive>
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Clasificación</th>
+              <th>Cantidad en Stock</th>
+              <th>Cantidad Stock Minimo</th>
+            </tr>
+          </thead>
+          <tbody>
+            {materiasPrimas && materiasPrimas.map(materiaPrima => (
+              <tr key={materiaPrima.materia_prima_id}>
+                <td>{materiaPrima.nombre}</td>
+                <td>{materiaPrima.clasificacion}</td>
+                <td>{materiaPrima.cantidad_stock}</td>
+                <td>{materiaPrima.cantidad_stock_minimo}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Card.Body>
+    </Card >
+    </Container>
+  );
+};
+
+export default Ingredients;
+
+
